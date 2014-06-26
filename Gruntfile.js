727db1697955c5ddea7d032232b8d8482ed31a0f
Bjorn Sandvik <bjorn.sandvik@gmail.com>
2014-06-26 22:57:36 +0200

    Moved lib folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
       },
       build: {
         src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        dest: 'dist/<%= pkg.name %>.js'
       }
     }
   });
